98b1bd1d60c0b32f0d5811aa363fbd8c702a29fd
faroke <yann.fo7@gmail.com>
2019-11-04 17:52:02 +0100

    Level up fonctionnel + correction bug

--- run.sh
+++ run.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
-level=0
 menu () {
 echo "		Usage : start ==> lancer l'entrainement niveau 1"
 read -p ">> " choice
 if [ choice="start" ]; then
-	play
+	play 1
 else
 	exit
 fi
 }
 play () {
 	clear
-	level=$(($level + 1))
-	NB_EXO="$(ls ressources/exercices/$level | wc -l)"
+	NB_EXO="$(ls ressources/exercices/$1 | wc -l)"
 	NB_EXO="$(shuf -i 1-$NB_EXO -n 1)"
 	echo "-------------------------------------------------------------------------------------------------"
-	echo "Exercice niveau $level"
-	echo "Nom de l'exercice : $(cat ressources/exercices/$level/$NB_EXO/exo | grep name | awk '{print $2}')"
-	echo "Prototypage de la fonction : $(cat ressources/exercices/$level/$NB_EXO/exo | grep  prototypage | awk '{print $2}')"	
+	echo "Exercice niveau $1"
+	echo "Nom de l'exercice : $(cat ressources/exercices/$1/$NB_EXO/exo | grep name | awk '{print $2}')"
+	#echo "Prototypage de la fonction : $(cat ressources/exercices/$1/$NB_EXO/exo | grep  prototypage | awk '{print $2}')"	
 	echo "-------------------------------------------------------------------------------------------------"
-	cat ressources/exercices/$level/$NB_EXO/doc
+	cat ressources/exercices/$1/$NB_EXO/doc
 	echo ""
 	echo "Une fois fini, veuillez entrer le chemin d'accès absolue"
 	read -p ">> " chemin
-	cat $chemin ressources/exercices/$level/$NB_EXO/test > .tmp/exotmp
-	racket .tmp/exotmp > .tmp/restmp
-	if  diff .tmp/restmp ressources/exercices/$level/$NB_EXO/compare; then
-		echo "reussi"
+	cat $chemin ressources/exercices/$1/$NB_EXO/test > tmp/exotmp
+	racket tmp/exotmp > tmp/restmp
+	if  diff tmp/restmp ressources/exercices/$1/$NB_EXO/compare; then
+		echo "reussi, lancement du prochaine exercice..."
+		sleep 2
+		play $(($1 + 1))
 	else
-		echo "failed"
+		clear
+		echo "failed, lancement du prochaine exercice..."
+		sleep 2
+		play $(($1 - 1))
 	fi
 }
 menu
+rm tmp/*
